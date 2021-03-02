ff735d87fe4784b63e8b24fbd3d90fe402d4bab7
“liyibass” <liyibass@mirrormedia.mg>
2021-03-02 22:55:06 +0800

    feat(Workflow): update workflowDetail animation

--- rex/client/components/WorkflowDetail.vue
+++ rex/client/components/WorkflowDetail.vue
@@ -1,31 +1,8 @@
 <template>
     <div class="WorkflowDetail">
-        <div class="WorkflowDetail__text" v-if="currentAnimate === 1">
-            依顧客心中畫面舉凡任何想法，<br />
-            團隊將有專人紀錄並提供回饋。
-        </div>
-
-        <div class="WorkflowDetail__text" v-else-if="currentAnimate === 2">
-            安排出勤單位前往案件現場，<br />
-            並以環境進行溝通及建議。
-        </div>
-
-        <div class="WorkflowDetail__text" v-else-if="currentAnimate === 3">
-            （待補上）
-        </div>
-
-        <div class="WorkflowDetail__text" v-else-if="currentAnimate === 4">
-            （待補上）
-        </div>
-
-        <div class="WorkflowDetail__text" v-else-if="currentAnimate === 5">
-            簽圖面完成後，設計師會再次與顧客進行全方面的校對， 並於後續進行備料且安排加工製程。<br />
-        </div>
-
-        <div class="WorkflowDetail__text" v-else-if="currentAnimate === 6">
-            將顧客心中的畫面進行實體化，並安排專業人員現場安裝， <br />
-            將最好的畫面留在客人的心中。
-        </div>
+        <transition name="showUp" mode="out-in">
+            <div class="WorkflowDetail__text" v-html="returnCurrentContext" :key="currentAnimate" />
+        </transition>
     </div>
 </template>
 
@@ -38,6 +15,27 @@ export default {
     props: {
         currentAnimate: Number,
     },
+    computed: {
+        returnCurrentContext() {
+            switch (this.currentAnimate) {
+                case 1:
+                    return '依客戶心中畫面舉凡任何想法，<br />團隊將有專人紀錄並提供回饋。'
+                case 2:
+                    return '安排出勤單位前往案件現場，<br />並以環境進行溝通及建議。'
+                case 3:
+                    return '當客戶與團隊想法一致時，依建議材質進入編價程序，<br />並透過會計人員擬定合約及合理價格提供給客戶。'
+                case 4:
+                    return '簽訂合約時，將會了解品牌精神及商空環境，<br />替客戶思考設計每個畫面及細節。'
+                case 5:
+                    return '簽圖面完成後，設計師會再次與客戶進行全方面的校對， 並於後續進行備料且安排加工製程。<br />'
+                case 6:
+                    return '將設計畫面進行實體化，並安排專業人員現場安裝，<br />將最好的畫面留在客人的心中。'
+
+                default:
+                    break
+            }
+        },
+    },
 }
 </script>
 
@@ -48,8 +46,19 @@ export default {
     font-size: 20px;
 
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     padding: 20px;
+}
+
+.showUp-enter-active,
+.showUp--active {
+    transition: all 1s;
+    transform: translateY(0%);
+}
+.showUp-enter, .showUp-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(40px);
 }
 </style>
