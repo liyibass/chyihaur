9de82471618baca54a9e79f79d0365f19f0f1617
LIYI <liyibass@gmail.com>
2021-08-28 20:11:50 +0800

    feat: add scroll-then-activate in poforlio card

--- client/components/UiPortfolioCard.vue
+++ client/components/UiPortfolioCard.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="UiPortfolioCard" @click="goToPortfolioPage" :style="{ flex: getRandomFlexSize }">
+    <div class="UiPortfolioCard" ref="card" @click="goToPortfolioPage" :style="{ flex: getRandomFlexSize }">
         <img :src="coverPhoto" :alt="portfolio.name" />
         <h1>{{ portfolio.name }}</h1>
     </div>
 </template>
 
 <script>
+import 'intersection-observer'
+import scrollama from 'scrollama'
 export default {
     props: {
         portfolio: {
@@ -49,6 +51,31 @@ export default {
             return Math.floor(Math.random() * 3) + 1
         },
     },
+
+    mounted() {
+        const selfDOM = this.$refs.card
+        // -------------------------------------------------------
+        // instantiate the scrollama
+        const portfolioCardScroller = scrollama()
+
+        // setup the instance, pass callback functions
+        portfolioCardScroller
+            .setup({
+                step: selfDOM,
+                offset: 0.4,
+            })
+            .onStepEnter((response) => {
+                // { element, index, direction }
+                response.element.classList.add('UiPortfolioCard_activate')
+            })
+            .onStepExit((response) => {
+                // { element, index, direction }
+                response.element.classList.remove('UiPortfolioCard_activate')
+            })
+
+        // setup resize event
+        window.addEventListener('resize', portfolioCardScroller.resize)
+    },
 }
 </script>
 
@@ -56,11 +83,14 @@ export default {
 .UiPortfolioCard {
     // flex: 1;
     position: relative;
-    width: 100%;
+    width: calc(100% - 10px);
     padding-top: 100%;
     margin: 5px;
     // max-height: 400px;
     // width: 100%;
+
+    background: rgba(0, 0, 0, 1);
+    overflow: hidden;
     @include atSmall {
         width: calc((100% - 30px) / 2);
         padding-top: calc((100% - 30px) / 2);
@@ -69,16 +99,15 @@ export default {
         width: calc((100% - 30px) / 3);
         padding-top: calc((100% - 30px) / 3);
     }
-    background: black;
-    overflow: hidden;
     img {
+        z-index: 0;
         position: absolute;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
         object-fit: cover;
-        opacity: 0.8;
+        filter: grayscale(100%);
         transition: all 0.5s linear;
     }
 
@@ -101,8 +130,31 @@ export default {
             opacity: 1;
         }
         img {
-            opacity: 0.5;
+            opacity: 0.8;
+            filter: grayscale(0%);
             transform: scale(1.05);
+        }
+    }
+
+    &_activate {
+        h1 {
+            opacity: 1;
+        }
+        img {
+            opacity: 0.8;
+            filter: grayscale(0%);
+            transform: scale(1.05);
+        }
+
+        @include atSmall {
+            h1 {
+                opacity: 0;
+            }
+            img {
+                opacity: 1;
+                filter: grayscale(100%);
+                transform: scale(1);
+            }
         }
     }
 }
